d23ba0051a4995dad9b5dcaa2347976f620db58f
Ilham Imaduddin <ilhamadun@gmail.com>
2018-03-13 11:32:39 +0700

    Create API Server
    The API serves these routes:
    - /camera-list
    - /recording-list
    - /playback
    - /live-stream
    
    A Session class is also created to provide a connection to the VMS. It's designed to support multiple VMS vendor that implements the VMSInterface.

---
+++ modules/api/src/session.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "hw_ivs.h"
+#include "session.h"
+
+namespace api = vms::api;
+
+std::shared_ptr<vms::VMSInterface> api::Session::login(
+    const std::string &ip, const std::string &username,
+    const std::string &password, const std::string &vendor) {
+  auto session = _get_session(ip);
+
+  if (session) {
+    if (_match(ip, username, password)) {
+      return session;
+    } else {
+      throw std::invalid_argument("Username and password does not match.");
+    }
+  } else {
+    try {
+      std::shared_ptr<vms::VMSInterface> vms = _create_vendor_vms(vendor);
+      vms->login(ip, 9900, username, password);
+
+      std::unique_lock<std::mutex> lock{_mutex};
+      _sessions.insert({ip, vms});
+      _auths.insert({ip, {username, password}});
+
+      return vms;
+    } catch (std::runtime_error) {
+      throw;
+    }
+  }
+}
+
+std::shared_ptr<vms::VMSInterface> api::Session::_create_vendor_vms(
+    const std::string &vendor) {
+  std::shared_ptr<vms::VMSInterface> vms{nullptr};
+
+  if (vendor == "huawei") {
+    vms = std::make_shared<vms::hwivs::HuaweiIVS>("./log");
+  }
+
+  return vms;
+}
+
+void api::Session::logout(const std::string &ip) {
+  auto session = _get_session(ip);
+
+  if (session) {
+    try {
+      session->logout();
+    } catch (std::runtime_error) {
+      throw;
+    }
+  } else {
+    throw std::invalid_argument("IP address does not exists in the session.");
+  }
+}
+
+bool api::Session::_match(const std::string &ip, const std::string &username,
+                          const std::string &password) {
+  auto auth = _auths.find(ip);
+
+  if (auth != _auths.end()) {
+    if (auth->second.username == username &&
+        auth->second.password == password) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+std::shared_ptr<vms::VMSInterface> api::Session::_get_session(
+    const std::string &ip) {
+  std::unique_lock<std::mutex> lock{_mutex};
+  auto session = _sessions.find(ip);
+
+  if (session == _sessions.end()) {
+    return nullptr;
+  }
+
+  return session->second;
+}
---
+++ modules/api/src/session.h
@@ -0,0 +1,45 @@
+#ifndef SESSION_H
+#define SESSION_H
+
+#include <memory>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+
+#include "vms_interface.h"
+
+namespace vms {
+namespace api {
+
+struct Auth {
+  std::string username;
+  std::string password;
+};
+
+class Session {
+ public:
+  std::shared_ptr<vms::VMSInterface> login(const std::string &ip,
+                                           const std::string &username,
+                                           const std::string &password,
+                                           const std::string &vendor);
+
+  void logout(const std::string &ip);
+
+ private:
+  std::shared_ptr<vms::VMSInterface> _create_vendor_vms(
+      const std::string &vendor);
+
+  std::shared_ptr<vms::VMSInterface> _get_session(const std::string &ip);
+
+  bool _match(const std::string &ip, const std::string &username,
+              const std::string &password);
+
+  std::unordered_map<std::string, std::shared_ptr<vms::VMSInterface>> _sessions;
+  std::unordered_map<std::string, Auth> _auths;
+  std::mutex _mutex;
+};
+
+}  // namespace api
+}  // namespace vms
+
+#endif

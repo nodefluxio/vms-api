dd12712508a67ddee7f23118a8d32e8d0e3c9cb9
Ilham Imaduddin <ilhamadun@gmail.com>
2018-08-10 22:28:53 +0700

    Force Huawei SDK to initialize and cleanup only once

--- modules/api/src/main.cpp
+++ modules/api/src/main.cpp
@@ -1,8 +1,14 @@
+#include "IVS_SDK.h"
 #include "server.h"
 
 int main() {
+  IVS_SDK_SetLogPath("./log");
+  IVS_SDK_Init();
+
   vms::api::Server server;
   server.run();
+
+  IVS_SDK_Cleanup();
 
   return 0;
 }
